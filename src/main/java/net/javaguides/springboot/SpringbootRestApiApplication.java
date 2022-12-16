7ad36d7f75f2065de2b9572ea977968bfbb5c092
Manish Thakur <manishhakur.in@hotmail.com>
2022-12-16 07:15:21 -0500

    feat: Added new code for repository.

--- src/main/java/net/javaguides/springboot/SpringbootRestApiApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootRestApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiApplication.class, args);
+    }
 
 }
