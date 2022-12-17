11ac50816c3c9e57e0e7274aa9ecee38965aae9d
Manish Thakur <manishhakur.in@hotmail.com>
2022-12-17 13:46:52 -0500

    Removed unwanted files.

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -3,15 +3,11 @@ package net.javaguides.springboot.controller;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
 import net.javaguides.springboot.entity.User;
-import net.javaguides.springboot.exception.ErrorDetails;
-import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -51,17 +47,4 @@ public class UserController {
         userService.deleteUser(userId);
         return new ResponseEntity<>("User deleted successfully!!!", HttpStatus.OK);
     }
-
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException,
-//                                                                        WebRequest webRequest) {
-//        ErrorDetails errorDetails = new ErrorDetails(
-//                LocalDateTime.now(),
-//                resourceNotFoundException.getMessage(),
-//                webRequest.getDescription(false),
-//                "USER_NOT_FOUND"
-//        );
-//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-//    }
-
 }
